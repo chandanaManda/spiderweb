faacabba9347af44d621812903b65807fd328518
Sai Chandana Manda <Sai Chandana Manda@DESKTOP-VG0UTEC>
2020-04-14 00:42:28 -0500

    first commit

---
+++ spiderweb/java/com/spiderweb/attendance/SpiderwebApplication.java
@@ -0,0 +1,13 @@
+package com.spiderweb.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiderwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiderwebApplication.class, args);
+	}
+
+}
---
+++ spiderweb/src/test/java/com/spiderweb/attendance/SpiderwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spiderweb.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiderwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
